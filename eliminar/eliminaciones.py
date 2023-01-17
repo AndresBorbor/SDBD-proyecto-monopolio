3e56aaff9840373397262587d66df5e163d4aef3
joemcara <94081168+joemcara@users.noreply.github.com>
2023-01-17 08:12:14 -0800

    Update eliminaciones.py

--- eliminar/eliminaciones.py
+++ eliminar/eliminaciones.py
@@ -9,3 +9,15 @@ def eliminarCompra(conecctionP,cod):
     conecctionP.execute(delete)
     print()
     print("se ejecuto correctamente")
+    
+ def eliminarTerreno(conecctionP,cod):
+    delete = "delete from terreno where id_terreno = "+str(cod)+""
+    conecctionP.execute(delete)
+    print()
+    print("se ejecuto correctamente")    
+
+def eliminarTarjeta(conecctionP,cod):
+    delete = "call sp_eliminarTarjeta("+str(cod)+")" 
+    conecctionP.execute(delete)
+    print()
+    print("se ejecuto correctamente")  
